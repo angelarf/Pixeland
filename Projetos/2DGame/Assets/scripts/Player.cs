71aeda2bd31c94e6a731af24bb0c25ce7ec32767
angelarf <angela.rod.fe@gmail.com>
2016-09-21 23:42:41 -0300

    Projeto Zaina adicionado, RF01 implementado

---
+++ Projetos/2DGame/Assets/scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float velocidade;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+        Movimentacao();
+	}
+
+	void Movimentacao(){
+        //Anda para frente
+		if(Input.GetAxisRaw("Horizontal") > 0){
+			transform.Translate (Vector2.right * velocidade * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+		}
+    }
+
+}
